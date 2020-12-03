e653338b028bb640de474a63ac61ee68a5ea9818
Cris Franco <cris.franco219@gmail.com>
2020-12-02 22:41:36 -0500

    added script functions

--- script.js
+++ script.js
@@ -24,3 +24,52 @@ var interval = setInterval(function() {
                          .substring(0,3).toUpperCase());
     $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
   }, 100);
+
+  function initPage() {
+
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nine.val(init9);
+  
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    ten.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    eleven.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelve.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    one.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    two.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    three.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    four.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    five.val(init5);
+    
+  } 
+  
+  function background () {
+        
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
